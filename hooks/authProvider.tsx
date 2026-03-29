f02ae52245e1195360c3481dc3750fc3ce1df5b8
Carlgm-22 <c.galvezmena@gmail.com>
2026-03-29 21:08:39 +0200

    feat(auth): create an auth provider to cheack globaly authentication concerns

---
+++ hooks/authProvider.tsx
@@ -0,0 +1,34 @@
+import { createContext, useContext, useState, ReactNode } from 'react';
+
+type AuthContextType = {
+  isAuthenticated: boolean;
+  login: () => void;
+  logout: () => void;
+};
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+// Hook personalizado para usarlo rápido y sin importar mil cosas
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth debe usarse dentro de un AuthProvider');
+  }
+  return context;
+}
+
+// El proveedor que envolverá tu app
+export function AuthProvider({ children }: { children: ReactNode }) {
+  // Por ahora lo simulamos con un boolean. 
+  // Más adelante, aquí leerás el token JWT de expo-secure-store.
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = () => setIsAuthenticated(true);
+  const logout = () => setIsAuthenticated(false);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
