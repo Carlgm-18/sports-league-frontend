409d2385a5cff43c84e889ac1c7c797ceda2236b
Carlgm-22 <c.galvezmena@gmail.com>
2026-03-29 21:40:15 +0200

    feat(ui): league main page

---
+++ app/ligas/[id]/index.tsx
@@ -0,0 +1,32 @@
+import { View, Text, StyleSheet } from 'react-native';
+import { useLocalSearchParams } from 'expo-router';
+
+export default function LeagueDetailsScreen() {
+  const { id } = useLocalSearchParams();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Detalles de la Liga</Text>
+      <Text style={styles.subtitle}>Has entrado a la liga con el ID: {id}</Text>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5F5F5',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#666',
+  },
+});
