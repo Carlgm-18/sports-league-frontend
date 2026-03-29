46a95ead7da6552fa5ba34a30addda62a5968ddc
Carlgm-22 <c.galvezmena@gmail.com>
2026-03-29 21:17:22 +0200

    fix(ui): adapt screens to new pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -16,11 +16,11 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <AuthProvider>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="ligas/[id]" options={{ headerShown: false }} />
+        </Stack>
+        <StatusBar style="auto" />
       </AuthProvider>
     </ThemeProvider>
   );
