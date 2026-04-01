74ff1f044723677b3d641b598be436f7c5d2f96d
Carlgm-22 <c.galvezmena@gmail.com>
2026-04-01 22:42:38 +0200

    chore(format): format code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,8 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
