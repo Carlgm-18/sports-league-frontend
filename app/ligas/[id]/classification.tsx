6d9d4e6aa9a854cc1a6be2b06d88eb8c25fc45d4
Carlgm-22 <c.galvezmena@gmail.com>
2026-04-09 15:39:09 +0200

    fix: delete unnecessary pages

--- app/ligas/[id]/classification.tsx
+++
@@ -1,6 +0,0 @@
-import { View, Text } from "react-native";
-
-// Tiene que ser así:
-export default function ClassificationScreen() {
-  return <View><Text>Clasificación</Text></View>;
-}
--- app/ligas/[id]/matches.tsx
+++
@@ -1,6 +0,0 @@
-import { View, Text } from "react-native";
-
-// Tiene que ser así:
-export default function MatchesScreen() {
-  return <View><Text>Partidos</Text></View>;
-}
