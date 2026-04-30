edc52f5253eaffb721e51f286122d7d7f4605635
Carlgm-22 <c.galvezmena@gmail.com>
2026-04-30 09:46:23 +0200

    feat: and use cases for /users/me and /users/login

--- services/UserService.ts
+++ services/UserService.ts
@@ -11,3 +11,12 @@ export const userRegister = async (
   newUser: UserCreateRequest,
 ): Promise<ApiResult<UserCreateResponse>> =>
   await apiClient.post<UserCreateResponse>('/users/register', newUser);
+
+export const getCurrentUser = async (): Promise<ApiResult<UserDetails>> =>
+  await apiClient.get<UserDetails>('/users/me');
+
+export const userLogin = async (
+  email: string,
+  password: string,
+): Promise<ApiResult<UserAuthResponse>> =>
+  await apiClient.post<UserAuthResponse>('/users/login', { email, password });
